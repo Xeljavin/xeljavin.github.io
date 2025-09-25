cf0ccc2805726112fe0fddfcdcf528b4640fbc57
Xeljavin <111482467+Xeljavin@users.noreply.github.com>
2025-09-25 08:49:50 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
+import { initTheme } from "./theme.js";
+// later: import { initLang } from "./lang.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+  initTheme();
+  // initLang(); // when language system is ready
+});
 
