7cd34777eba1ce1b3a8a4e3c80eb3477a2e9c925
Xeljavin <111482467+Xeljavin@users.noreply.github.com>
2025-09-27 22:03:34 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { initTheme } from "./theme.js";
-// later: import { initLang } from "./lang.js"
+import { initLang } from "./lang.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  initTheme();
-  // initLang(); // when language system is ready
+    initTheme();
+    initLang("en"); // default language
 });
-
